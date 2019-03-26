a3f04fd8c0630738e84c640bdf548cb18a75db26
tomas.a.cerezo@outlook.com <tomas.a.cerezo@outlook.com>
2019-03-26 13:24:56 -0300

    Guia 0 - Ejercicio 2

--- Guia 0 - Ej 2)/Program.cs
+++ Guia 0 - Ej 2)/Program.cs
@@ -10,6 +10,22 @@ namespace Guia_0___Ej_2_
     {
         static void Main(string[] args)
         {
+            Console.Write("Ingrese el primer número entero: ");
+            string cadena1 = Console.ReadLine();
+            int.TryParse(cadena1, out int num1);
+            Console.Write("Ingrese el segundo número entero:");
+            string cadena2 = Console.ReadLine();
+            int.TryParse(cadena2, out int num2);
+
+            Console.WriteLine(num1 + num2);
+            Console.WriteLine(num1 - num2);
+            Console.WriteLine(num1 * num2);
+            Console.WriteLine(Math.Pow(num1,num2));
+            Console.WriteLine(num1 % num2);
+
+            Console.ReadKey();
+
+
         }
     }
 }
